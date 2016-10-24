216669d622288e541edb737da9b4b1457b0f898a
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-10-24 23:19:16 +0300

    Add jstl, Add find mock service.

--- src/main/java/com/example/mvc/sample/controllers/HomeController.java
+++ src/main/java/com/example/mvc/sample/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.mvc.sample.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.mvc.samle.entities.data.Project;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/home")
-	public String sayHello() {
+	public String home(Model model) {
+		Project project = new Project();
+		project.setDescription("This is project sponsored by Nasa");
+		project.setSponsor("NASA");
+		project.setName("Simple Project 1");
+		
+		model.addAttribute("currentProject", project);
 		return "home";
 	}
 }
--- src/main/java/com/example/mvc/sample/controllers/ProjectController.java
+++ src/main/java/com/example/mvc/sample/controllers/ProjectController.java
@@ -1,30 +1,42 @@
 package com.example.mvc.sample.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.mvc.samle.entities.data.services.ProjectService;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+	@Autowired
+	ProjectService service;
 
-	@RequestMapping(value = "add", method = RequestMethod.GET)
+	@RequestMapping(value = "/find", method = RequestMethod.GET)
+	public String findProject(Model model) {
+		model.addAttribute("projects", service.findAll());
+		return "projects";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String addProject() {
 		return "project_add";
 	}
 
-	@RequestMapping(value = "add", method = RequestMethod.POST)
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String saveProject() {
 		return "project_add";
 	}
 
-	@RequestMapping(value = "add", method = RequestMethod.POST, params = { "authorized_funds=multi" })
+	@RequestMapping(value = "/add", method = RequestMethod.POST, params = { "authorized_funds=multi" })
 	public String multiProject() {
 		System.out.println("MultiProject");
 		return "project_add";
 	}
 
-	@RequestMapping(value = "add", method = RequestMethod.POST, params = { "authorized_funds=multi", "special" })
+	@RequestMapping(value = "/add", method = RequestMethod.POST, params = { "authorized_funds=multi", "special" })
 	public String specialProject() {
 		System.out.println("MultiProject");
 		return "project_add";
