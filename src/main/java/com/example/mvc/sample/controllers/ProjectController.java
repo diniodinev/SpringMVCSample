60edd0d28c4b3f380c296d43643d9a8755be78e8
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-10-25 20:48:40 +0300

    Add more project controller handler methods.

--- src/main/java/com/example/mvc/sample/controllers/ProjectController.java
+++ src/main/java/com/example/mvc/sample/controllers/ProjectController.java
@@ -3,6 +3,7 @@ package com.example.mvc.sample.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +14,12 @@ import com.example.mvc.samle.entities.data.services.ProjectService;
 public class ProjectController {
 	@Autowired
 	ProjectService service;
+
+	@RequestMapping(value="/{projectId}")
+	public String findbyId(final Model model, @PathVariable ("projectId") Long projectId){
+		model.addAttribute("project", service.find(projectId));
+		return "project";
+	}
 
 	@RequestMapping(value = "/find", method = RequestMethod.GET)
 	public String findProject(Model model) {
