99daf092fe2c7e62dcd1073518cbab9970e61826
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-10-25 21:34:27 +0300

    Set @requestparam

--- src/main/java/com/example/mvc/sample/controllers/ProjectController.java
+++ src/main/java/com/example/mvc/sample/controllers/ProjectController.java
@@ -1,11 +1,15 @@
 package com.example.mvc.sample.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.mvc.samle.entities.data.services.ProjectService;
 
@@ -15,8 +19,8 @@ public class ProjectController {
 	@Autowired
 	ProjectService service;
 
-	@RequestMapping(value="/{projectId}")
-	public String findbyId(final Model model, @PathVariable ("projectId") Long projectId){
+	@RequestMapping(value = "/{projectId}")
+	public String findbyId(final Model model, @PathVariable("projectId") Long projectId) {
 		model.addAttribute("project", service.find(projectId));
 		return "project";
 	}
@@ -28,12 +32,16 @@ public class ProjectController {
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String addProject() {
+	public String addProject(HttpSession session) {
+		session.setAttribute("token", "12345");
 		return "project_add";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String saveProject() {
+	public String saveProject(@RequestParam(name = "sponsor") String sponsor, HttpSession session,
+			HttpServletRequest request) {
+		System.out.println(session.getAttribute("token"));
+		System.out.println("The request " + sponsor);
 		return "project_add";
 	}
 
