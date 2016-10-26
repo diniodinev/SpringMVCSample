2d5513c68b29506720285fd822783979ddc444c0
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-10-26 19:15:53 +0300

    Add changes

--- src/main/java/com/example/mvc/samle/entities/data/Project.java
+++ src/main/java/com/example/mvc/samle/entities/data/Project.java
@@ -35,6 +35,7 @@ public class Project {
 	}
 
 	public void setName(String name) {
+		System.out.println("Spring databinds the name");
 		this.name = name;
 	}
 
@@ -94,4 +95,12 @@ public class Project {
 		this.type = type;
 	}
 
+	@Override
+	public String toString() {
+		return "Project [projectId=" + projectId + ", name=" + name + ", description=" + description + ", sponsor="
+				+ sponsor + ", authorizedHours=" + authorizedHours + ", authorizedFunds=" + authorizedFunds + ", year="
+				+ year + ", special=" + special + ", type=" + type + "]";
+	}
+
+	
 }
--- src/main/java/com/example/mvc/sample/controllers/ProjectController.java
+++ src/main/java/com/example/mvc/sample/controllers/ProjectController.java
@@ -1,16 +1,16 @@
 package com.example.mvc.sample.controllers;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.mvc.samle.entities.data.Project;
 import com.example.mvc.samle.entities.data.services.ProjectService;
 
 @Controller
@@ -38,23 +38,8 @@ public class ProjectController {
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String saveProject(@RequestParam(name = "sponsor") String sponsor, HttpSession session,
-			HttpServletRequest request) {
-		System.out.println(session.getAttribute("token"));
-		System.out.println("The request " + sponsor);
+	public String saveProject(@ModelAttribute Project project) {
+		System.out.println(project);
 		return "project_add";
 	}
-
-	@RequestMapping(value = "/add", method = RequestMethod.POST, params = { "authorized_funds=multi" })
-	public String multiProject() {
-		System.out.println("MultiProject");
-		return "project_add";
-	}
-
-	@RequestMapping(value = "/add", method = RequestMethod.POST, params = { "authorized_funds=multi", "special" })
-	public String specialProject() {
-		System.out.println("MultiProject");
-		return "project_add";
-	}
-
 }
