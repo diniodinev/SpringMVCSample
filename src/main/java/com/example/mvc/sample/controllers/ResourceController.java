06091c43189ce50a8de6f7f7a5e7d2c3924aa8d4
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-10-30 16:29:16 +0200

    Add Model attributes at method level in order ot pass common infomration in the model for the whole controller

--- src/main/java/com/example/mvc/sample/controllers/ResourceController.java
+++ src/main/java/com/example/mvc/sample/controllers/ResourceController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.mvc.samle.entities.data.Resource;
 
@@ -17,24 +18,37 @@ public class ResourceController {
 
 	@RequestMapping("/add")
 	public String add(Model model) {
-		List<String> options = new LinkedList<>(
-				Arrays.asList(new String[] { "Material", "Other", "Staff", "Technical Equipment" }));
-
-		List<String> radios = new LinkedList<>(Arrays.asList(new String[] { "Hours", "Piece", "Tons" }));
-		model.addAttribute("radioOptions", radios);
-
-		List<String> checks = new LinkedList<>(
-				Arrays.asList(new String[] { "Lead Time", "Special Rate", "Requires Approval" }));
-		model.addAttribute("checkOptions", checks);
-
-		model.addAttribute("typeOptions", options);
-		model.addAttribute("resource", new Resource());
 		return "resource_add";
+	}
+
+	@ModelAttribute(value = "resource")
+	public Resource resource() {
+		return new Resource();
+	}
+
+	@ModelAttribute(value = "typeOptions")
+	public List<String> options() {
+		return new LinkedList<>(Arrays.asList(new String[] { "Material", "Other", "Staff", "Technical Equipment" }));
+	}
+
+	@ModelAttribute(value = "radioOptions")
+	public List<String> radioOptions() {
+		return new LinkedList<>(Arrays.asList(new String[] { "Hours", "Piece", "Tons" }));
+	}
+
+	@ModelAttribute(value = "checkOptions")
+	public List<String> checkOptions() {
+		return new LinkedList<>(Arrays.asList(new String[] { "Lead Time", "Special Rate", "Requires Approval" }));
 	}
 
 	@RequestMapping("/save")
 	public String save(@ModelAttribute Resource resource) {
 		System.out.println(resource);
 		return "redirect:/resource/add";
+	}
+	
+	@RequestMapping(value="/find", method = RequestMethod.GET)
+	public String find(@ModelAttribute Resource resource) {
+		return "resource_add";
 	}
 }
