902a6b2f24044b9984b3938530878581b4c21629
Dinyo Dinev <dinyo.dinev@gmail.com>
2016-11-01 23:03:29 +0200

    commit the controller

--- src/main/java/com/example/mvc/sample/controllers/ProjectController.java
+++ src/main/java/com/example/mvc/sample/controllers/ProjectController.java
@@ -2,9 +2,14 @@ package com.example.mvc.sample.controllers;
 
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.mvc.samle.entities.data.Project;
 import com.example.mvc.samle.entities.data.services.ProjectService;
+import com.example.mvc.samle.entities.validators.ProjectValidator;
 
 @Controller
 @RequestMapping("/project")
@@ -33,18 +39,29 @@ public class ProjectController {
 
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String addProject(Model model) {
-		model.addAttribute("types", new ArrayList<String>(){{
-			add("");
-			add("Single Year");
-			add("Multi Year");
-		}});
+		model.addAttribute("types", new ArrayList<String>() {
+			{
+				add("");
+				add("Single Year");
+				add("Multi Year");
+			}
+		});
 		model.addAttribute("project", new Project());
 		return "project_add";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String saveProject(@ModelAttribute("project") Project project) {
-		System.out.println(project);
+	public String saveProject(@Valid @ModelAttribute("project") Project project, Errors errors) {
+		if (!errors.hasErrors()) {
+			System.out.println("Everythink is ok wit the project name.");
+		} else {
+			System.out.println("Project name is invalid.");
+		}
 		return "project_add";
+	}
+
+	@InitBinder
+	public void projectValidator(WebDataBinder binder) {
+		binder.addValidators(new ProjectValidator());
 	}
 }
