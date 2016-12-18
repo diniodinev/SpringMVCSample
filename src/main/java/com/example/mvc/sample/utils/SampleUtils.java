f4fa1e1665d3ab3790beec2dee53a54763f8d3f7
Dinyo Ilchev Dinev <dinyo.dinev@gmail.com>
2016-12-18 16:29:29 +0200

    comit comment

--- src/main/java/com/example/mvc/sample/utils/SampleUtils.java
+++ src/main/java/com/example/mvc/sample/utils/SampleUtils.java
@@ -2,9 +2,9 @@ package com.example.mvc.sample.utils;
 
 /**
  * Sample util class which role is to store common methods.
- * @author Hannibal
- *
+ * 
  */
+
 public class SampleUtils {
 
 	public static String toLowerCase(String word) {
